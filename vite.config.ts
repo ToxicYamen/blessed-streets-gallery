f92ff83c3fb3dfd6751fe4b2e723bccf5586062f
ToxicYamen <57713229+ToxicYamen@users.noreply.github.com>
2025-04-25 21:21:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     rollupOptions: {
-      external: ['@tsparticles/react', 'canvas'],
+      external: ['canvas'],
     },
   },
 }));
